315f210375cc8dfa0b5a38e41e169579bef87142
Twaal <55928366+Twaal@users.noreply.github.com>
2019-12-12 14:00:15 -0500

    Add files via upload
    variables for latitude / longitude added.
    Marker only at location of plane.
    Line from origin to plane.

--- misc/MapsMarkerActivity.java
+++ misc/MapsMarkerActivity.java
@@ -9,6 +9,14 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+
+//import java.net.URLEncoder;
+//import com.mashape.unirest.http.HttpResponse;
+//import com.mashape.unirest.http.JsonNode;
+//import com.mashape.unirest.http.Unirest;
+
 
 /**
  * An activity that displays a Google map with a marker (pin) to indicate a particular location.
@@ -37,13 +45,39 @@ public class MapsMarkerActivity extends AppCompatActivity
      * Play services inside the SupportMapFragment. The API invokes this method after the user has
      * installed Google Play services and returned to the app.
      */
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        //plane = createBitmap()
         // Add a marker in Sydney, Australia,
         // and move the map's camera to the same location.
-        LatLng sydney = new LatLng(-33.852, 151.211);
-        googleMap.addMarker(new MarkerOptions().position(sydney)
-                .title("Marker in Sydney"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        double origLat = 40.7128;
+        double origLong = -74.0060; //-73.935242
+
+        double locLat = 42.360;
+        double locLong = -71.058;
+
+
+        LatLng origin = new LatLng(origLat, origLong);
+        LatLng location = new LatLng(locLat, locLong);
+        googleMap.addMarker(new MarkerOptions().position(location)
+                .title("Marker in Boston"));
+        //MarkerOptions bostonMarker = new MarkerOptions().position(boston).title("Marker in Boston");
+        //bostonMarker.icon("planeMarker2.ico");
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location,14f));
+
+       // LatLng norway = new LatLng(59.913, 10.752);
+        //googleMap.addMarker(new MarkerOptions().position(norway).title("Marker in Oslo"));
+
+
+        googleMap.addPolyline(new PolylineOptions().add(location).add(origin));
+
+    }
+
+    public void showNorway(GoogleMap googleMap){
+        LatLng norway = new LatLng(59.913, 10.752);
+        googleMap.addMarker(new MarkerOptions().position(norway).title("Marker in Oslo"));
     }
 }
